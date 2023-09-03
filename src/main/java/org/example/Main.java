5927b537bfe2a38564e8994dfeaa1dfc9e6e800c
lonchik2020 <krasleo@gmail.com>
2023-09-03 19:24:36 +0300

    added for to branch_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello world!1");
+        for(int i=0; i<10; i++){
+            System.out.println("branch_1");
+        }
         System.out.println("Hello world!2");
         System.out.println("Hello world!3");
     }
